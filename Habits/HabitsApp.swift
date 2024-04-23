e69bf5b80bad69fb65ab5ea64e409269119b6487
Jonas Bondesson <jonasbondesson@yahoo.com>
2024-04-23 11:09:14 +0200

    first commit

---
+++ Habits/HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsApp.swift
+//  Habits
+//
+//  Created by Jonas Bondesson on 2024-04-23.
+//
+
+import SwiftUI
+
+@main
+struct HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
